31ba892afa1305d88c516be1e7a80d9b78ea9fc5
Patryk Koreń <koren.patryk@gmail.com>
2024-05-09 19:06:29 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,16 @@
 sudo apt update
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt install build-essential btop fish nvim tmux curl git nodejs
+sudo apt install build-essential btop fish nvim tmux curl git nodejs npm ripgrep
 
-sudo apt install chromium alacritty
+curl -fsSL https://bun.sh/install | bash
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -L https://foundry.paradigm.xyz | bash
+source /home/patryk/.bashrc
+foundryup
+
+sudo apt install chromium alacritty vlc krita inkscape blender
+
+which fish
+chsh patryk
